0a3d8c8435537730eeb31309acbe224aa8005d37
liumin <970568830@qq.com>
2021-06-13 03:31:46 +0800

    gx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from "react";
 import { AppProps } from "next/app";
 import { ThemeProvider } from "@material-ui/core/styles";
-import { ApolloProvider } from "@apollo/client";
+// import { ApolloProvider } from "@apollo/client";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { useApollo } from "@/lib/apollo/client";
+// import { useApollo } from "@/lib/apollo/client";
 import theme from "@/config/theme/default";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const apolloClient = useApollo(pageProps.initialApolloState);
+  // const apolloClient = useApollo(pageProps.initialApolloState);
 
   useEffect(() => {
     const jssStyles = document.querySelector("#jss-server-side");
@@ -18,11 +18,11 @@ export default function App({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ApolloProvider>
+    // <ApolloProvider client={apolloClient}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+    // </ApolloProvider>
   );
 }
